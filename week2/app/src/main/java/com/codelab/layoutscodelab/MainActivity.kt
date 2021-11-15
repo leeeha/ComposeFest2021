b1371ab7933d64f7d3a4f13f54cdbcd179a730a5
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-15 17:35:18 +0900

    Decoupled ConstraintLayout

--- week2/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
+++ week2/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
@@ -19,10 +19,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.compose.atLeast
 import coil.annotation.ExperimentalCoilApi
@@ -45,25 +48,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LargeConstraintLayout() {
-    ConstraintLayout {
-        val text = createRef()
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
 
-        val guideline = createGuidelineFromStart(fraction = 0.5f)
-        Text(
-            "This is a very very very very very very very long text",
-            Modifier.constrainAs(text) {
-                linkTo(start = guideline, end = parent.end)
-                width = Dimension.preferredWrapContent.atLeast(100.dp)
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
             }
-        )
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin= margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
     }
 }
 
 @Preview
 @Composable
-fun LargeConstraintLayoutPreview() {
+fun DecoupledConstraintLayoutPreview() {
     LayoutsCodeLabTheme {
-        LargeConstraintLayout()
+        DecoupledConstraintLayout()
     }
 }
